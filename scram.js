915b7c7d980c54f0cdbe6962bce538cf13f75c48
matt manes <mattmanes@pm.me>
2024-10-17 12:57:51 -0500

    fix: detect different container used on mobile

--- scram.js
+++ scram.js
@@ -2,13 +2,21 @@
 var intervalId = setInterval(scram, 100)
 var maxChecks = 100
 var numChecks = 0
+var cleared = false
 function scram () {
   var container = document.getElementById('credential_picker_container')
+  var mobileContainer = document.getElementById('credential_picker_iframe')
 
-  if (container != null || numChecks >= maxChecks) {
-    clearInterval(intervalId)
+  if (container != null) {
     container.remove()
+    cleared = true
+  } else if (mobileContainer != null) {
+    mobileContainer.remove()
+    cleared = true
   } else {
     ++numChecks
+  }
+  if (numChecks >= maxChecks || cleared) {
+    clearInterval(intervalId)
   }
 }
